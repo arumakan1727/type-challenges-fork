627f849ae39a2dd37cb58f14b4868ab6a50a6213
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2024-09-17 07:34:01 +0900

    solve: 00010-medium-tuple-to-union

--- playground/02-medium/00010-medium-tuple-to-union.ts
+++ playground/02-medium/00010-medium-tuple-to-union.ts
@@ -20,7 +20,7 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type TupleToUnion<T> = any;
+type TupleToUnion<T extends readonly unknown[]> = T[number];
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils';
