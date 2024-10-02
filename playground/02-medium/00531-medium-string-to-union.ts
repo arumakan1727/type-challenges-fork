e50b250111c3a3b1987ec5dbd801baeea07f0e6a
Arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2024-10-02 09:21:38 +0900

    solve: 00531-medium-string-to-union (#40)

--- playground/02-medium/00531-medium-string-to-union.ts
+++ playground/02-medium/00531-medium-string-to-union.ts
@@ -19,7 +19,9 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type StringToUnion<T extends string> = any;
+type StringToUnion<T extends string> = T extends `${infer Head}${infer Tail}`
+  ? Head | StringToUnion<Tail>
+  : never;
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils';
